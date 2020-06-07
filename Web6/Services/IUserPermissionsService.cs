fa60a060ccbd97378a2dbc13c57fbd88d35e35a6
is108 <alexey21s@rambler.ru>
2020-06-07 22:54:30 +0700

    добавлена 6 лабораторная по вебам

---
+++ Web6/Services/IUserPermissionsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Web6.Models;
+
+namespace Web6.Services
+{
+    public interface IUserPermissionsService
+    {
+        Boolean CanEditPost(Post post);
+
+        Boolean CanEditPostComment(PostComment postComment);
+    }
+}
